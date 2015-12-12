81dcd01f25acae99344336c0ccd771c8827cafec
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-12-12 15:13:34 -0600

    Newsletter submit button now linked to standard form

---
+++ js/newsletter_button.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+/* email validation stuff i'll get to later
+    var $emailField = $('#newsletter-email');
+    var email = $emailField.val();
+  var re = /a-z/;
+  $emailField.keypress(function() {
+    console.log(re.typeOf);
+  });
+  */
+  $('#newsletter-submit').click(function() {
+
+  });
+});
